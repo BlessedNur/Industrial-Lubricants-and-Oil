e6d1c1173ebd08e55c85d0caf183a0f2f8421d7c
NurBlessedProj <blessednur67@gmail.com>
2024-07-30 17:23:10 +0100

    adjusted

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,32 +1,9 @@
-"use client";
-import React from "react";
-import AliceCarousel from "react-alice-carousel";
-import "react-alice-carousel/lib/alice-carousel.css";
+import React from 'react'
 
-const handleDragStart = (e) => e.preventDefault();
-
-const items = [
-  <img src="path-to-img" onDragStart={handleDragStart} role="presentation" />,
-  <img src="path-to-img" onDragStart={handleDragStart} role="presentation" />,
-  <img src="path-to-img" onDragStart={handleDragStart} role="presentation" />,
-];
-
-function Page() {
+function page() {
   return (
-    <AliceCarousel
-      mouseTracking
-      items={items}
-      responsive={{
-        0: {
-          items: 1,
-        },
-        1024: {
-          items: 3,
-          itemsFit: "contain",
-        },
-      }}
-    />
-  );
+    <div>page</div>
+  )
 }
 
-export default Page;
+export default page
