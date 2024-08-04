a6bf7adf13dbf84aef37b0984676fefc4ccce563
NurBlessedProj <blessednur67@gmail.com>
2024-08-04 14:47:10 +0100

    suspense added

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -3,7 +3,7 @@ import Banner from "@/components/Banner/Banner";
 import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
 import { useRouter, useSearchParams } from "next/navigation";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import style from "./page.module.css";
 import { allProducts } from "@/components/allProducts";
 import Image from "next/image";
@@ -54,94 +54,96 @@ function Page() {
   };
 
   return (
-    <section>
-      <Navbar />
-      <Banner
-        presentPage={"products"}
-        text={"products"}
-        height={"90svh"}
-        extraText={"This is where you can browse products in this store."}
-        diff={true}
-        img={"/images/20221202-5S7A6269-scaled.jpg"}
-      />
-      <div className={style.main}>
-        <div className={style.category}>
-          <h1>
-            Choose your <br /> category
-          </h1>
-          <select name="" value={category} id="" onChange={handleChange}>
-            <option value="">Category</option>
-            <option value="mobil 1">Mobil 1</option>
-            <option value="castrol">Castrol</option>
-            <option value="total quartz">Total Quartz</option>
-            <option value="valvoline">Valvoline</option>
-            <option value="gear oil">Gear oils</option>
-            <option value="grease">Grease</option>
-            <option value="tractor hydraulic fluids">
-              Tractor Hydraulic Fluids
-            </option>
-            <option value="transmission">Transmission</option>
-            <option value="tire">TIRES</option>
-            <option value="filteration">Filters</option>
-            <option value="engine coolant">Engine Coolant</option>
-            <option value="engine lubricants">Engine Lubricants</option>
-            <option value="industrial lubricants">Industrial Lubricants</option>
-          </select>
-          <div className={style.search}>
-            <input
-              type="text"
-              value={searchValue}
-              onChange={handleSearch}
-              placeholder="Search.."
-            />
-            <i className="fa fa-search" aria-hidden="true"></i>
+    <Suspense fallback={<div>Loading...</div>}>
+      <section>
+        <Navbar />
+        <Banner
+          presentPage={"products"}
+          text={"products"}
+          height={"90svh"}
+          extraText={"This is where you can browse products in this store."}
+          diff={true}
+          img={"/images/20221202-5S7A6269-scaled.jpg"}
+        />
+        <div className={style.main}>
+          <div className={style.category}>
+            <h1>
+              Choose your <br /> category
+            </h1>
+            <select name="" value={category} id="" onChange={handleChange}>
+              <option value="">Category</option>
+              <option value="mobil 1">Mobil 1</option>
+              <option value="castrol">Castrol</option>
+              <option value="total quartz">Total Quartz</option>
+              <option value="valvoline">Valvoline</option>
+              <option value="gear oil">Gear oils</option>
+              <option value="grease">Grease</option>
+              <option value="tractor hydraulic fluids">
+                Tractor Hydraulic Fluids
+              </option>
+              <option value="transmission">Transmission</option>
+              <option value="tire">TIRES</option>
+              <option value="filteration">Filters</option>
+              <option value="engine coolant">Engine Coolant</option>
+              <option value="engine lubricants">Engine Lubricants</option>
+              <option value="industrial lubricants">Industrial Lubricants</option>
+            </select>
+            <div className={style.search}>
+              <input
+                type="text"
+                value={searchValue}
+                onChange={handleSearch}
+                placeholder="Search.."
+              />
+              <i className="fa fa-search" aria-hidden="true"></i>
+            </div>
           </div>
-        </div>
-        <div className={style.product}>
-          {displayedProducts.length === 0 ? (
-            <p
-              style={{
-                textTransform: "uppercase",
-                fontStyle: "italic",
-                fontSize: "30px",
-                color: "white",
-                fontWeight: "800",
-              }}
-            >
-              No products found
-            </p>
-          ) : (
-            displayedProducts.map((product, index) => (
-              <div
-                key={index}
-                className={style.productItem}
-                onClick={() => {
-                  navigate.push(`/${encodeURIComponent(product.name)}`);
+          <div className={style.product}>
+            {displayedProducts.length === 0 ? (
+              <p
+                style={{
+                  textTransform: "uppercase",
+                  fontStyle: "italic",
+                  fontSize: "30px",
+                  color: "white",
+                  fontWeight: "800",
                 }}
               >
-                <div className={style.image}>
-                  <img src={product.image} alt={product.name} />
+                No products found
+              </p>
+            ) : (
+              displayedProducts.map((product, index) => (
+                <div
+                  key={index}
+                  className={style.productItem}
+                  onClick={() => {
+                    navigate.push(`/${encodeURIComponent(product.name)}`);
+                  }}
+                >
+                  <div className={style.image}>
+                    <img src={product.image} alt={product.name} />
+                  </div>
+                  <p>{product.name}</p>
+                  <div className={style.icon}>
+                    <Image
+                      width={1000}
+                      height={1000}
+                      src={"/images/Icon-feather-arrow-down-right.svg"}
+                    />
+                  </div>
                 </div>
-                <p>{product.name}</p>
-                <div className={style.icon}>
-                  <Image
-                    width={1000}
-                    height={1000}
-                    src={"/images/Icon-feather-arrow-down-right.svg"}
-                  />
-                </div>
-              </div>
-            ))
+              ))
+            )}
+          </div>
+          {displayedProducts.length < searchedProducts.length && (
+            <button className={style.loadMore} onClick={loadMoreProducts}>
+              Load more products
+            </button>
           )}
         </div>
-        {displayedProducts.length < searchedProducts.length && (
-          <button className={style.loadMore} onClick={loadMoreProducts}>
-            Load more products
-          </button>
-        )}
-      </div>
-      <Footer />
-    </section>
+        <Footer />
+      </section>
+    </Suspense>
   );
 }
 
