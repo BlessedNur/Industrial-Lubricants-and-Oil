458fa0d675416efe8a09136730f75bd345dde877
NurBlessedProj <blessednur67@gmail.com>
2024-08-04 14:36:58 +0100

    error fixed

--- src/app/brands/page.jsx
+++ src/app/brands/page.jsx
@@ -7,7 +7,7 @@ import { brands } from "@/components/brands";
 import style from "./page.module.css";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-function page() {
+function Page() {
   const navigate = useRouter();
   return (
     <section>
@@ -48,4 +48,4 @@ function page() {
   );
 }
 
-export default page;
+export default Page;
