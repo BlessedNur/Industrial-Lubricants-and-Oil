890668e1e324f6c34be1775f0e5d7203eaae6ae7
NurBlessedProj <blessednur67@gmail.com>
2024-08-05 21:04:32 +0100

    whatapp contact ixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -81,7 +81,7 @@ function Footer() {
           </div>
           <div className={style.line}>
             <i class="fa-brands fa-whatsapp"></i>{" "}
-            <Link href="">+1-208-278-6426</Link>
+            <Link href="https://wa.me/12082786426">+1-208-278-6426</Link>{" "}
           </div>
           <div className={style.line}>
             <i className="fa fa-envelope" aria-hidden="true"></i>
